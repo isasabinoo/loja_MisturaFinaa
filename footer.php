f905266847a51b4e6f638674771b6c09bfe1dda9
isasabinoo <163453755+isasabinoo@users.noreply.github.com>
2024-03-14 15:22:58 -0300

    Add files via upload
    Commit realizado com alterações aplicadas na aula do dia 14 de março

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div>
+
+    <!-- container abrigara o rodapé da página-->
+    <div class="container-fluid text-center bg-dark text-light fixed-bottom">
+        <p>Sistema Web desenvolvido por Isabelle</p>
+    </div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include("header.php") ?>
+
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+
+        <?php include("footer.php") ?>
+
+    
